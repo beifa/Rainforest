f9da39273e8dbe0b29f4f969be67bb409b0bb4e8
beifa <beifabeifa@mail.ru>
2020-12-18 12:39:44 +0300

    ReduceLROnPlateau changed param,fix error tr_idx and vl_idx,add accuracy metric,pablic score 0.776

--- src/train.py
+++ src/train.py
@@ -10,12 +10,12 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader,Dataset, RandomSampler
 from tqdm import tqdm
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, average_precision_score
 from sklearn.metrics import label_ranking_average_precision_score
 
 from model import Res50
 from dataset import RFDataset
-from utils import lwlrap, visual_train_result
+from utils import lwlrap, visual_train_result, FocalLoss
 
 from torch.cuda import amp
 
@@ -36,21 +36,6 @@ def set_seed(seed=0):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
 
-"""
-    parser = argparse.ArgumentParser(description='Train model')
-    parser.add_argument('-f', '--fold', type=int, help = 'add fold number', default=0)    
-    parser.add_argument('-e', '--epoch', type=int, help = 'add count epoch', default= 10)    
-    parser.add_argument('-n', '--num_workers', type=int, help = 'num_workers', default= 4)
-    parser.add_argument('-m', '--model', type=str, help = 'name model to train : res50, eff(name, out)', default = 'res50')
-    parser.add_argument('-l', '--loss_func', type=str, help = 'loss func : BCEWithLogitsLoss, BCELoss, dice_loss, FocalLoss, MixedLoss ', default = 'BCEWithLogitsLoss')
-    parser.add_argument('-d', '--debag', type=bool, help = 'small data set', default = False)
-    
-
-
-    pars = parser.parse_args()
-
-"""
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--kernel', type=str, required=True, help = 'name_model_size_epoch_typefold_scheduler_mixadd')
@@ -64,12 +49,34 @@ def parse_args():
     args, _ = parser.parse_known_args()
     return args
 
+def accuracy(pred: np.array, target: np.array)->np.array:    
+    # add same idea by accuracy metric
+    corrects = 0
+    tt, tar_idx = torch.max(target, dim= 1)
+    aa, ans_idx = torch.max(pred, dim= 1) #dim=0 values, if dim =1 return max value and index max value
+    # print('tr-->', tt, tar_idx)
+    # print('as-->', aa,ans_idx)
+    
+    for i in range(0, len(ans_idx)):
+        if ans_idx[i] == tar_idx[i]:
+            corrects = corrects + 1  
+    return corrects
+
+def fuck_auc(target:np.array, probs: np.array)-> float:
+     # ValueError: Only one class present in y_true. ROC AUC score is not defined in that case.
+    try:
+        auc = roc_auc_score(target, probs) 
+    except ValueError:
+        auc = 0
+    return auc
+
 def  train(model, loader, loss_f, optimizer, scaler):
     model.train()
     bar = tqdm(loader)
     train_loss = []
     train_target = []
     train_prob = []
+    correct = []
     for (img, target) in bar:
         optimizer.zero_grad()
         if args.scaler:
@@ -78,9 +85,9 @@ def  train(model, loader, loss_f, optimizer, scaler):
                 img, target = img.to(device), target.to(device)
                 y_ = model(img)
                 pred = loss_f(y_, target)
-                scaler.scale(pred).backward()
-                scaler.step(optimizer)
-                scaler.update()            
+            scaler.scale(pred).backward()
+            scaler.step(optimizer)
+            scaler.update()            
         else:
             img, target = img.to(device), target.to(device)
             y_ = model(img)
@@ -89,13 +96,15 @@ def  train(model, loader, loss_f, optimizer, scaler):
             optimizer.step()
         
         train_loss.append(pred.detach().cpu().numpy())
-        prob = nn.Sigmoid()(y_)        
+        prob = nn.Sigmoid()(y_)     
+        correct.append(accuracy(y_, target))    
         train_prob.append(prob.detach().cpu())
         train_target.append(target.detach().cpu())            
     
     probs =  torch.cat(train_prob).numpy()
-    target = torch.cat(train_target).numpy()   
-    auc = roc_auc_score(target, probs)
+    target = torch.cat(train_target).numpy()
+    auc = fuck_auc(target, probs)
+    print(f'TR Correct : {sum(correct)} / {len(target)}')
     return train_loss, auc
 
 
@@ -103,6 +112,7 @@ def val_train(model, loader, loss_f):
     val_loss = []
     target_temp = []
     probs = []
+    correct = []
     model.eval()
     with torch.no_grad():
         bar = tqdm(loader)
@@ -112,7 +122,9 @@ def val_train(model, loader, loss_f):
             
             pred = loss_f(y_, target)            
             prob = nn.Sigmoid()(y_)  
-            
+
+            correct.append(accuracy(y_, target)) 
+
             val_loss.append(pred.detach().cpu())        
             probs.append(prob.detach().cpu())
             target_temp.append(target.detach().cpu())   
@@ -121,32 +133,38 @@ def val_train(model, loader, loss_f):
     probs =  torch.cat(probs).numpy()
     target = torch.cat(target_temp).numpy()
    
-    auc = roc_auc_score(target, probs)
+    auc = fuck_auc(target, probs) 
+
+    pr = average_precision_score(target, probs, average='weighted')
+    pr_m = average_precision_score(target, probs, average='micro')
+
     lraps = label_ranking_average_precision_score(target, probs)
 
     score_class, weight = lwlrap(target, probs)
     score_loss = (score_class * weight).sum()
-
-    return val_loss, auc, lraps, score_loss
+    print(f'VL Correct : {sum(correct)} / {len(target)}')
+    return val_loss, auc, lraps, score_loss, pr, pr_m, sum(correct)
 
 
 def showtime(model, f: int, df: pd.DataFrame, label:pd.DataFrame, scaler):
     start = time.ctime().replace('  ', ' ').replace(' ', '_')    
     print('Fold: ', f)
+    tr_idx = df[args.fold_type] != f
+    vl_idx = df[args.fold_type] == f
+
     if args.DEBUG:
         print('DEBUG....................')
         start = 'DEBUG_' + start
-        args.epoch = 5
-        args.size = 64        
-        print(args)
-        tr_data = df[df.sfold !=f].sample(200).reset_index(drop=True)
-        vl_data = df[df.sfold ==f].sample(200).reset_index(drop=True)
+        args.epoch = 1               
+        print(args)        
+        tr_label = label.loc[tr_idx].sample(200).reset_index(drop=True)
+        vl_label = label.loc[vl_idx].sample(200).reset_index(drop=True)
     else:
-        tr_data = df[df[args.fold_type] != f].reset_index(drop=True)
-        vl_data = df[df[args.fold_type] == f].reset_index(drop=True)
-    
-    tr_dataset = RFDataset(PATH_NPY, label.loc[tr_data.index], size = args.size)
-    vl_dataset = RFDataset(PATH_NPY, label.loc[vl_data.index], size = args.size)
+        tr_label = label.loc[tr_idx].reset_index(drop=True)
+        vl_label = label.loc[vl_idx].reset_index(drop=True)
+    print(tr_label.shape, vl_label.shape)
+    tr_dataset = RFDataset(PATH_NPY, tr_label, size = args.size)
+    vl_dataset = RFDataset(PATH_NPY, vl_label, size = args.size)
     
     tr_loader = DataLoader(tr_dataset, batch_size=args.batch, num_workers=args.n_workers,
                            sampler=RandomSampler(tr_dataset))
@@ -157,57 +175,58 @@ def showtime(model, f: int, df: pd.DataFrame, label:pd.DataFrame, scaler):
     # need addd pos_weight 
     pos_weights = torch.ones(24)
     pos_weights = pos_weights * 24    
-    loss_f = nn.BCEWithLogitsLoss(reduction="mean", pos_weight=pos_weights).to(device)       
-    optimizer = optim.Adam(model.parameters())#, lr = 3e-5)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=3, verbose=True)
-
+    loss_f = nn.BCEWithLogitsLoss(reduction="mean", pos_weight=pos_weights).to(device)
+    # loss_f = FocalLoss()       
+    optimizer = optim.Adam(model.parameters())#, lr = 0.001, weight_decay=0.0001)
+    # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=0.9, mode='min', patience=4, verbose=True)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',factor=0.9, patience=3, verbose=True)
 
     auc_max = 0
-    
+    correct_max = 0    
     for ep in range(args.epoch):
         print('Epoch: ', ep + 1)
         
         train_loss, auc_train = train(model, tr_loader, loss_f, optimizer, scaler)
-        val_loss, auc, lraps, score_loss = val_train(model, vl_loader, loss_f)
+        val_loss, auc, lraps, score_loss, pr,pr_m, correct = val_train(model, vl_loader, loss_f)
 
         log = time.ctime().replace('  ', ' ').replace(' ', '_') + ',' + f'Fold:{f},Epoch:{ep},lr:{optimizer.param_groups[0]["lr"]:.7f},Auc_val:{auc:.5f},Auc_train:{auc_train:.5f},LRAPS:{lraps:.5f},lwlrap:{score_loss:.5f},train_loss:{np.mean(train_loss):.5f},val_loss:{val_loss:.5f}'
+               
         print(log)
         to_log = start + '.' + f'log_{args.kernel}.txt'          
         with open(os.path.join(PATH_LOGS, to_log), 'a') as file:
             file.write(log + '\n')
 
         if auc > auc_max:
-            print(f'auc_max: {auc} --> {auc_max}). Saving model ...')
+            print(f'auc_max: {auc} --> {auc_max}, PRw: {pr}, PRmicro: {pr_m}). Saving model ...')
             torch.save(model.state_dict(), os.path.join(PATH_MODEL, f'{args.kernel}_best_fold_{f}.pth'))
-            auc_max = auc       
+            auc_max = auc      
         
-        scheduler.step(val_loss) 
+        scheduler.step(val_loss)         
     visual_train_result(to_log)   
     torch.save(model.state_dict(), os.path.join(PATH_MODEL, f'{args.kernel}_final_fold_{f}.pth'))
     torch.cuda.empty_cache()   
 
 if __name__ == "__main__":
-    """
-     
 
-    params = {
-        'SEED': 13,
-        'batch_size': 32,
-        'lr': 1e-4,
-        'num_workers' : pars.num_workers,
-        'epoch': pars.epoch,
-        'fold': pars.fold,
-        'model': pars.model,
-        'loss_func' : pars.loss_func
-    }
+    tp = pd.read_csv(os.path.join(PATH_CSV, 'train_tp.csv'))
+    fp = pd.read_csv(os.path.join(PATH_CSV, 'train_fp.csv'))
+    df = pd.concat((tp, fp))
+    df = df.sample(frac = 1).reset_index(drop=True)
     
-    """
+    from sklearn.model_selection import StratifiedKFold
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=13)
+    
+    df['kfold'] = -1
+    for f, (tr, vl) in enumerate(skf.split(df, df.species_id)):
+        print(len(tr), len(vl))
+        df.loc[vl, 'kfold'] = f  
 
-    df = pd.read_csv(os.path.join(PATH_CSV, 'gfold_sfold_df.csv')) # two way folds
+    # df = pd.read_csv(os.path.join(PATH_CSV, 'gfold_sfold_df.csv')) # two way folds   
     label = pd.read_csv(os.path.join(PATH_CSV, 'label.csv')) # two way folds
+    # label = pd.read_csv(os.path.join(PATH_CSV, 'label_tp.csv'))
     set_seed(SEED)
     args = parse_args()
     scaler = amp.GradScaler()  
     model = Res50()
-    fold = 4
+    fold = 0
     showtime(model, fold, df, label, scaler)
