990718ab5112ea26f5ca7ff156f47d562b5f9ef2
beifa <beifabeifa@mail.ru>
2020-12-18 12:44:12 +0300

    fix error code

--- src/model.py
+++ src/model.py
@@ -6,21 +6,23 @@ class Res50(nn.Module):
     
     def __init__(self):
         super(Res50, self).__init__()        
-        self.model = pretrainedmodels.__dict__['resnet50'](pretrained = 'imagenet')
-        # list(resnet50.children())[:-2]
-        self.in_f = self.model.last_linear.in_features
-        self.model.last_linear = nn.Identity()
-        self.l1 = nn.Linear(self.in_f, 24)
-        
+        self.model = pretrainedmodels.__dict__['resnet50'](pretrained = 'imagenet')        
+        in_ch = self.model.last_linear.out_features  
+        self.myfc = nn.Linear(in_ch, 24)
+        self.model.fc = nn.Identity()   
+       
+    def extract(self, x):
+        x = self.model(x)
+        return x
         
     def forward(self, x):
-        # for test add dim becous 20 bin
+        # for test add dim because 10 bin
         if len(torch.squeeze(x).shape) == 4:
             x = torch.squeeze(x)
         else:
             x
         bs, _, _, _ = x.shape
-        x = self.model(x)       
+        x = self.extract(x)     
         x = x.view(bs, -1)
-        x = self.l1(x)
+        x = self.myfc(x)
         return x
