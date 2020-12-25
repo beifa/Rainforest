18281a5d54cd62ac224ed9f06344b87ead7f2d13
beifa <beifabeifa@mail.ru>
2020-12-25 22:42:59 +0300

    add EffB3 model

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 import torch
+import geffnet
 import torch.nn as nn
 import pretrainedmodels
 
@@ -26,3 +27,27 @@ class Res50(nn.Module):
         x = x.view(bs, -1)
         x = self.myfc(x)
         return x
+
+class EffB3(nn.Module):
+
+  def __init__(self):
+    super(EffB3, self).__init__()
+    self.model = geffnet.create_model('efficientnet_b3', pretrained=True) 
+    in_ch = self.model.classifier.in_features #1536
+    self.myfc = nn.Sequential(nn.Linear(in_ch, 24))
+    self.model.classifier = nn.Identity()   
+      
+  def extract(self, x):        
+    x = self.model(x)        
+    return x
+      
+  def forward(self, x):    
+    if len(torch.squeeze(x).shape) == 4:
+        x = torch.squeeze(x)
+    else:
+        x
+    bs, _, _, _ = x.shape    
+    x = self.extract(x)     
+    # x = x.view(bs, -1)   
+    x = self.myfc(x)
+    return x  
