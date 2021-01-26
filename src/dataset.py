f3184ff6c3a58dd9e6de2e050bbf6a61392db9a6
beifa <beifabeifa@mail.ru>
2021-01-26 15:23:45 +0300

    change test dataset, loads data

--- src/dataset.py
+++ src/dataset.py
@@ -8,8 +8,6 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 from utils import mono_to_color 
 
-PATH_ZIP = '../input/train_img.zip'
-
 # class RFDataset(Dataset):
 
 #     def __init__(self, path, data, size, transform = False):
@@ -34,13 +32,14 @@ PATH_ZIP = '../input/train_img.zip'
 #         target = self.data.loc[index].values[1:]        
 #         return torch.tensor(img).float(),torch.tensor(target.astype(np.float16)).float() #check
 
+
 # check work with zip
 class RFDataset(Dataset):
 
-    def __init__(self, data, size, transform = False):        
+    def __init__(self, data, path, size, transform = False):        
         self.size = size     
         self.transform = transform
-        self.zipdata = np.load(PATH_ZIP)
+        self.zipdata = np.load(path)
         self.data = data # list name npy,
         
     def __len__(self):
@@ -62,40 +61,7 @@ class RFDataset(Dataset):
         img = img / 255.0
         img = img.transpose(2, 0, 1).astype(np.float32)        
              
-        return torch.tensor(img).float(),torch.tensor(target.astype(np.float16)).float() #check
-
-
-# class RFDataset_test(Dataset):
-#     """
-#     .npy cut by 6sec one file have shape(10, *, *)    
-#     """
-
-#     def __init__(self, path, size, transform = None):
-#         self.data = np.asarray(path)
-#         self.size = size        
-#         self.transform = transform
-        
-    
-#     def __len__(self):
-#         return self.data.shape[0]    
-    
-#     def __getitem__(self, index):
-#         temp = []
-#         name = self.data[index]   
-#         img = np.load(name)          
-#         name_img = os.path.basename(name)
-        
-#         if self.transform is not None:
-#             pass
-
-#         for i in range(len(img)):
-#             image = mono_to_color(img[i]) 
-#             if self.size is not None:
-#                 image = cv2.resize(image, (576, self.size))
-#             image = image / 255.0
-#             image = image.transpose(2, 0, 1).astype(np.float32) 
-#             temp.append(image)        
-#         return torch.tensor(temp).float(), name_img.split('.')[0]
+        return torch.tensor(img).float(),torch.tensor(target.astype(np.float16)).float() 
 
 
 class RFDataset_test(Dataset):
@@ -103,30 +69,26 @@ class RFDataset_test(Dataset):
     .npy cut by 6sec one file have shape(10, *, *)    
     """
 
-    def __init__(self, size, transform = None):
-      self.p1 = np.load('../input/test_img_p1.zip')
-      self.p2 = np.load('../input/test_img_p2.zip')
-      
-      self.size = size
-      self.transform = transform
+    def __init__(self, name, size, transform = None):
+        p1 = glob.glob(f'../input/{name}/test_img_p1/test/*.npy')
+        p2 = glob.glob(f'../input/{name}/test_img_p2/test/*.npy')
+        p1.extend(p2)
+        self.p1 = p1
+        
+        self.size = size
+        self.transform = transform
         
     
     def __len__(self):        
-        return len(self.p1.files[1:]) + len(self.p2.files[1:])
+        return len(self.p1) 
     
     def __getitem__(self, index):
-        temp = []
-        
-        if index <= 999:
-          name = self.p1.files[1:][index]          
-          name_file = os.path.basename(name)
-          img = self.p1[name]
-          # print(name, name_file)
-        else:
-          name = self.p2.files[1:][index-1000]
-          name_file = os.path.basename(name)
-          img = self.p2[name]                
-        
+        temp = []        
+       
+        name = self.p1[index]          
+        name_file = os.path.basename(name)
+        img = np.load(name)
+  
         if self.transform is not None:
             pass
 
@@ -138,6 +100,4 @@ class RFDataset_test(Dataset):
             image = image.transpose(2, 0, 1).astype(np.float32) 
             temp.append(image)        
         return torch.tensor(temp).float(), name_file.split('.')[0]
-
-
     
