11934e0c2ef6501e5b6804d4ec75d65d943f392a
beifa <beifabeifa@mail.ru>
2021-02-16 18:50:05 +0300

    add fix bag GradualWarmupSchedulerV2

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
+from warmup_scheduler import GradualWarmupScheduler  # https://github.com/ildoonet/pytorch-gradual-warmup-lr
 
 
 def mono_to_color(X: np.ndarray,
@@ -133,3 +134,21 @@ def rand_window(data: np.array, version = None)->np.array:
     len_img = (start + cut_len) - start
     assert len_img == cut_len, f'error len {start}, {start + cut_len}'
     return data[:, start: start + cut_len]  
+
+
+# Fix Warmup Bug
+class GradualWarmupSchedulerV2(GradualWarmupScheduler):
+    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+        super(GradualWarmupSchedulerV2, self).__init__(optimizer, multiplier, total_epoch, after_scheduler)
+    def get_lr(self):
+        if self.last_epoch > self.total_epoch:
+            if self.after_scheduler:
+                if not self.finished:
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                    self.finished = True
+                return self.after_scheduler.get_lr()
+            return [base_lr * self.multiplier for base_lr in self.base_lrs]
+        if self.multiplier == 1.0:
+            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+        else:
+            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
